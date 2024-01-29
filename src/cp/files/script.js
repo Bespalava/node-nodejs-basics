f194e5e4b19007e6172007df70a0ecbe658fe036
ybespalava <ybespalava@gmail.com>
2024-01-30 00:22:09 +0100

    feat: script.js

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -4,9 +4,14 @@ console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = chunk.toString();
+  if (chunkStringified.includes('CLOSE')) process.exit(0);
+  process.stdout.write(`Received from master process: ${chunkStringified}\n`);
 };
 
 process.stdin.on('data', echoInput);
+process.on('message', (message) => {
+  console.log(`Received from master process (IPC): ${message}`);
+});
+
+process.send('Child process is ready');
