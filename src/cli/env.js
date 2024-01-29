faabca3a7a43e16136f8132314e40a0a9cc0257c
ybespalava <ybespalava@gmail.com>
2024-01-29 23:54:55 +0100

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
     const rssVariables = Object.keys(process.env).filter(key => key.startsWith('RSS_'));
+
     rssVariables.forEach(key => {
       const value = process.env[key];
       console.log(`RSS_${key}=${value};`);
